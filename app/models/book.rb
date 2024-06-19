2b8226e71a6cb60022d8202886358b196d9a63fd
fg0fg0fg <158440003+fg0fg0fg@users.noreply.github.com>
2024-06-19 04:00:19 +0000

    [update]notification

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,13 +2,14 @@ class Book < ApplicationRecord
   belongs_to :user
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+  has_many :notifications, as: :notifiable, dependent: :destroy
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
-  
+
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
-  
+
   def self.looks(search, word)
     if search ==  "perfect"
       @book = Book.where("title LIKE?", "#{word}")
@@ -20,6 +21,13 @@ class Book < ApplicationRecord
       @book = Book.where("title LIKE?", "%#{word}")
     else
       @book = Book.all
+    end
+  end
+
+  #通知機能
+  after_create do
+    user.followers.each do |follower|
+      notifications.create(user_id: follower.id)
     end
   end
 end
